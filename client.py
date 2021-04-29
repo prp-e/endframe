56202ad36b0c36a549a689706339ae9fd889eea3
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-29 13:18:15 +0430

    minor updates

--- client.py
+++ client.py
@@ -8,15 +8,17 @@ initial_list = os.listdir(directory)
 while True:
     temp_list = os.listdir(directory)
     for item in temp_list:
-        """ File addition detection """ 
         if item not in initial_list:
+            """ File addition detection """ 
             print(f'New item {item} found, adding to the database!')
             initial_list.append(item)
             print("Database updated successfully.")
-        """ File deletion detection """
-        if item in initial_list and item not in temp_list:
-            print(f"Deletion of {item} detected.")
+
+    for item in initial_list:
+        if item not in temp_list:
+            """ File deletion detection """
+            print(f"Deletion of {item} detected")
             initial_list.remove(item)
             print("Database updated successfully")
     
-    time.sleep(30)
+    time.sleep(15)
